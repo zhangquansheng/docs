3c66c348aef1a28bb329d3c88ebd7a2564b7ec10
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-06-15 15:04:39 +0800

    版本发布脚本

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,10 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:zhangquansheng/zhangquansheng.github.io.git master
+# 如果发布到 https://zhangquansheng.github.io/
+git push -f git@github.com:zhangquansheng/zhangquansheng.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:zhangquansheng/zhangquansheng.github.io.git/zhengcheng-doc master:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
