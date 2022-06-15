fbd5f0ec45595832bf3eea721d54ea79aa1c577a
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-06-15 14:28:48 +0800

    版本发布脚本

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd zhengcheng-doc/.vuepress/docs
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:zhangquansheng/zhangquansheng.github.io.git master
+# git push -f git@github.com:zhangquansheng/zhangquansheng.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:zhangquansheng/zhengcheng-doc.github.io.git master:gh-pages
 
 cd -
