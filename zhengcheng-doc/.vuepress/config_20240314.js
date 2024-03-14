43ba491532aaf4994509e47e6ed2fd3148b5eed8
zhangquansheng <952547584@qq.com>
2024-03-14 17:33:41 +0800

    重新整理技术文档

---
+++ zhengcheng-doc/.vuepress/config_20240314.js
@@ -0,0 +1,433 @@
+module.exports = {
+    port: "80",
+    ga: "UA-85414008-1",
+    markdown: {
+        lineNumbers: true,
+        externalLinks: {
+            target: '_blank', rel: 'noopener noreferrer'
+        }
+    },
+    head: [
+        ['meta', {
+            name: 'keywords',
+            content: '新征程框架,新征程,zhengcheng.plus'
+        }]
+    ],
+    title: "新征程框架",
+    description: "简化开发 效率至上",
+    lastUpdated: "上次更新",
+    plugins: [
+        '@vuepress/back-to-top',
+        ['@vuepress/last-updated', {
+            transformer: (timestamp, lang) => {
+                // 不要忘了安装 moment (npm install moment)
+                const moment = require('moment')
+                moment.locale(lang)
+                return moment(timestamp).fromNow()
+            }
+        }],
+        ['@vuepress/active-header-links', {
+            sidebarLinkSelector: '.sidebar-link',
+            headerAnchorSelector: '.header-anchor'
+        }]
+    ],
+    themeConfig: {
+        activeHeaderLinks: false,
+        logo: '/img/logo.png',
+        nav: [
+            {text: '首页', link: '/'},
+            {text: '指南', link: '/guide/'},
+            {
+                text: '好站',
+                items: [
+                    {
+                        text: '凤凰架构',
+                        link: 'https://icyfenix.cn/'
+                    },
+                    {
+                        text: 'Spring Framework Documentation',
+                        link: 'https://docs.spring.io/spring-framework/docs/current/reference/html/index.html'
+                    },
+                    {
+                        text: 'Spring Boot 2.1.18.RELEASE',
+                        link: 'https://docs.spring.io/spring-boot/docs/2.1.18.RELEASE/reference/html/'
+                    },
+                    {
+                        text: 'Spring Cloud Greenwich.SR5',
+                        link: 'https://cloud.spring.io/spring-cloud-static/Greenwich.SR5/single/spring-cloud.html'
+                    },
+                    {text: 'MyBatis-Plus', link: 'http://mybatis.plus/'},
+                    {
+                        text: 'ShardingSphere',
+                        link: 'https://shardingsphere.apache.org/document/current/cn/overview/'
+                    },
+                    {text: 'MVN REPOSITORY', link: 'https://mvnrepository.com/'},
+                    {
+                        text: 'Algorithms',
+                        link: 'https://www.cs.usfca.edu/~galles/visualization/Algorithms.html'
+                    },
+                    {text: 'Hutool', link: 'https://www.hutool.cn/'},
+                    {text: '力扣（LeetCode）', link: 'https://leetcode.cn/'},
+                    {text: 'Arthas（阿尔萨斯）', link: 'https://github.com/alibaba/arthas'},
+                    {text: 'ProcessOn', link: 'https://www.processon.com/'},
+                    {
+                        text: 'P3C',
+                        link: 'https://github.com/alibaba/p3c'
+                    },
+                    {
+                        text: 'stack overflow',
+                        link: 'https://stackoverflow.com/'
+                    },
+                    {
+                        text: 'redisson-spring-boot-starter',
+                        link: 'https://github.com/redisson/redisson/tree/master/redisson-spring-boot-starter'
+                    },
+                ]
+            },
+            {text: '更新日志', link: '/change-log/'},
+            {text: 'GitHub', link: 'https://github.com/zhangquansheng/zhengcheng-parent'},
+        ],
+        sidebar: [
+            {
+                title: '开发指南',
+                collapsable: false,
+                children: [
+                    './guide/',
+                    './guide/getting-started',
+                    './guide/web-core',
+                    './guide/mybatis-plus',
+                    './guide/dynamic-datasource',
+                    './guide/cache',
+                    './guide/idempotent',
+                    './guide/feign',
+                    './guide/async',
+                    './guide/mapstruct',
+                    // './guide/apollo',
+                    './guide/nacos',
+                    './guide/socketio',
+                    './guide/nacos-eureka',
+                    './guide/log'
+                ]
+            },
+            {
+                title: 'Spring Cloud 大型线上商城项目实战',
+                collapsable: false,
+                children: [
+                    './mall/',
+                    './mall/sa-token',
+                    './mall/product'
+                ]
+            },
+            {
+                title: 'Java 基础',
+                collapsable: false,
+                children: [
+                    './java/java8-func',
+                    './java/java8-stream',
+                    './java/java-basic-datatypes',
+                    './java/generics',
+                    './java/reference',
+                    './java/abstract-interface',
+                    './java/HashMap',
+                    './java/ConcurrentHashMap',
+                    './java/jvm',
+                    './java/gc',
+                    './java/cms',
+                    './java/g1'
+                ]
+            },
+            // {
+            //     title: 'Maven 基础',
+            //     collapsable: false,
+            //     children: [
+            //         './maven/'
+            //     ]
+            // },
+            {
+                title: '并发编程',
+                collapsable: false,
+                children: [
+                    './concurrent/volatile',
+                    './concurrent/synchronized',
+                    './concurrent/cas',
+                    './concurrent/AQS',
+                    './concurrent/thread-local',
+                    './concurrent/TransmittableThreadLocal',
+                    './concurrent/thread-pool-executor',
+                    './concurrent/Future',
+                    './concurrent/CompletableFuture'
+                ]
+            },
+            {
+                title: 'Spring',
+                collapsable: false,
+                children: [
+                    './spring/',
+                    './spring/dependency-injection',
+                    './spring/beans',
+                    './spring/circular-dependencies',
+                    './spring/beans-annotation-config',
+                    './spring/transaction-declarative',
+                    './spring/aop',
+                    './spring/validation'
+                ]
+            },
+            {
+                title: 'Spring Boot',
+                collapsable: false,
+                children: [
+                    './spring-boot/',
+                    './spring-boot/configuration-properties',
+                    './spring-boot/redis-pubsub',
+                    './spring-boot/spring-boot-starter',
+                    './spring-boot/transaction-after',
+                    './spring-boot/ContiPerf'
+                ]
+            },
+            {
+                title: 'Spring Cloud',
+                collapsable: false,
+                children: [
+                    './spring-cloud/feign',
+                    './spring-cloud/feign-dubbo',
+                    './spring-cloud/ribbon',
+                    './spring-cloud/refresh-scope',
+                    './spring-cloud/apollo-zuul',
+                    './spring-cloud/Hystrix',
+                    './spring-cloud/gw'
+                ]
+            },
+            {
+                title: 'MySQL',
+                collapsable: false,
+                children: [
+                    './mysql/',
+                    './mysql/Infrastructure',
+                    './mysql/b-plus-index',
+                    './mysql/ICP',
+                    './mysql/locking',
+                    './mysql/transaction',
+                    './mysql/sql-standard',
+                    './mysql/database-design-paradigm',
+                    './mysql/innodb_trx',
+                    './mysql/datetime',
+                    './mysql/update',
+                    './mysql/delete',
+                    './mysql/sql-execute',
+                    './mysql/index-invalid',
+                    './mysql/department-highest-salary',
+                    './mysql/having',
+                    './mysql/MySQL-8',
+                    './mysql/limit_order_by'
+                ]
+            }, {
+                title: 'Redis',
+                collapsable: false,
+                children: [
+                    './redis/',
+                    './redis/data-types',
+                    './redis/memory',
+                    './redis/reactor',
+                    './redis/three-problems-of-cache',
+                    './redis/cache-database-double-write-consistency',
+                    // './redis/redisson',
+                    './redis/redisson-lock',
+                    './redis/big-key',
+                    './redis/bitmap',
+                    './redis/delay-queue',
+                    './redis/pub-sub',
+                    './redis/J2Cache',
+                ]
+            }, {
+                title: 'Elasticsearch',
+                collapsable: false,
+                children: [
+                    './es/',
+                    './es/inverted-index',
+                    './es/Query-DSL.md',
+                    './es/tokenizer',
+                    './es/aggregations',
+                    './es/restHighLevelClient'
+                ]
+            }, {
+                title: 'Kafka',
+                collapsable: false,
+                children: [
+                    './kafka/',
+                    './kafka/spring-kafka',
+                    './kafka/multiple-kafka-config',
+                    './kafka/message-sequencing',
+                    './kafka/spring-kafka-retry'
+                ]
+            }, {
+                title: 'RocketMQ',
+                collapsable: false,
+                children: [
+                    './rocketmq/',
+                    './rocketmq/message-queue.md',
+                    './rocketmq/consumer',
+                    './rocketmq/message-idempotent',
+                    './rocketmq/rocketmq-spring',
+                    './rocketmq/rocketmq-spring-apache',
+                    './rocketmq/transaction',
+                    './rocketmq/delay'
+                ]
+            },
+            // {
+            //     title: 'Flink',
+            //     collapsable: false,
+            //     children: [
+            //         './flink/'
+            //     ]
+            // },
+            {
+                title: 'ZooKeeper',
+                collapsable: false,
+                children: [
+                    './zk/',
+                    './zk/zab',
+                    './zk/watcher',
+                    './zk/lock'
+            }, {
+                ]
+                title: 'MyBatis',
+                collapsable: false,
+                children: [
+                    './mybatis/',
+                    './mybatis/cache',
+                    './mybatis/mybatis-crud',
+                    './mybatis/MyBatis-Plus',
+                    './mybatis/DataPermissionHandler',
+                ]
+            }, {
+                title: 'Apache ShardingSphere',
+                collapsable: false,
+                children: [
+                    './shardingsphere/',
+                    './shardingsphere/concept',
+                    './shardingsphere/getting-started',
+                    './shardingsphere/sharding'
+                ]
+            }, {
+                title: '数据结构与算法',
+                collapsable: false,
+                children: [
+                    './algorithms/',
+                    './algorithms/algorithms-think',
+                    './algorithms/binary-tree',
+                    './algorithms/sort',
+                    './algorithms/data-structure',
+                    './algorithms/dynamic-programming',
+                    './algorithms/lru'
+                    // './algorithms/balanced-tree',
+                    // './algorithms/sliding-window',
+                ]
+            }, {
+                title: '面向对象设计模式',
+                collapsable: false,
+                children: [
+                    './design-patterns/',
+                    './design-patterns/factory',
+                    './design-patterns/abstract-factory',
+                    './design-patterns/builder',
+                    './design-patterns/adapter',
+                    './design-patterns/singleton',
+                    './design-patterns/strategy',
+                    './design-patterns/template',
+                    './design-patterns/facade',
+                    './design-patterns/flyweight',
+                    './design-patterns/proxy',
+                    './design-patterns/chain-of-responsibility',
+                    './design-patterns/mediator',
+                    './design-patterns/principle',
+                    './design-patterns/ifelse-1',
+                    './design-patterns/ifelse-2',
+                    './design-patterns/dynamic-proxy'
+                ]
+            }, {
+                title: '综合话题&源码分析',
+                collapsable: false,
+                children: [
+                    './digging-deeper/docker-in-action',
+                    './digging-deeper/docker-compose',
+                    './digging-deeper/Clean-Code',
+                    './digging-deeper/naming',
+                    './digging-deeper/DateTime',
+                    './digging-deeper/plugin',
+                    './digging-deeper/io',
+                    './digging-deeper/oom',
+                    './digging-deeper/copy-properties',
+                    './digging-deeper/sign-auth',
+                    './digging-deeper/mdc',
+                    './digging-deeper/distributed-lock',
+                    './digging-deeper/http-long-polling',
+                    './digging-deeper/xxl-job',
+                    './digging-deeper/xxl-rpc',
+                    // './digging-deeper/meituan-leaf',
+                    './digging-deeper/cpu100',
+                    './digging-deeper/bitmap',
+                    './digging-deeper/multipart-upload',
+                    './digging-deeper/arthas',
+                    './digging-deeper/distributed-transaction',
+                    './digging-deeper/sequence-diagram',
+                    './digging-deeper/auto-cancel',
+                    './digging-deeper/duplicate-pay',
+                    './digging-deeper/Middle-Platform',
+                    './digging-deeper/visit-incr',
+                    './digging-deeper/seckill',
+                    './digging-deeper/ddd'
+                ]
+            },  {
+                title: '微服务框架体系',
+                collapsable: false,
+                children: [
+                    './Microservice-Architecture/',
+                    './Microservice-Architecture/Architecture-design',
+                    './Microservice-Architecture/Architecture',
+                    './Microservice-Architecture/limit-request',
+                    './Microservice-Architecture/Nacos',
+                    './Microservice-Architecture/Availability'
+                ]
+            }, {
+                title: '计算机网络基础',
+                collapsable: false,
+                children: [
+                    './network/',
+                ]
+            },
+            // {
+            //     title: '项目管理',
+            //     collapsable: false,
+            //     children: [
+            //         './PM/project-flow',
+            //     ]
+            // },
+            // {
+            //     title: '代码人生',
+            //     collapsable: false,
+            //     children: [
+            //         './career/',
+            //         './career/biz',
+            //         './career/book-list',
+            //     ]
+            // },
+            {
+                title: 'FAQ',
+                collapsable: false,
+                children: [
+                    './faq/jackson-serializable',
+                    './faq/hikaricp-connection-is-not-available',
+                    './faq/hikaricp-config',
+                ]
+            }, {
+                title: '更新日志',
+                collapsable: false,
+                children: [
+                    './change-log/'
+                ]
+            }
+        ]
+    }
+};
+
+
--- zhengcheng-doc/README.md
+++ zhengcheng-doc/README.md
@@ -1,7 +1,7 @@
 ---
 home: true
 heroImage: /img/logo.png
-heroText: 新征程框架项目
+heroText: 新征程软件技术平台
 tagline: 简化开发 效率至上
 actionText: 快速上手 →
 actionLink: /guide/
@@ -12,5 +12,5 @@ features:
   details: 让我们在开发业务时能够专注于业务的编写而不必过多的关注框架的配置。
 - title: 代码规范
   details: 促进团队合作，降低维护成本，减少低级BUG，有助于代码审查。
-footer: Apache License 2.0 | Copyright © 2020-2023 张全胜
+footer: Apache License 2.0 | Copyright © 2020-2024 张全胜
 ---
